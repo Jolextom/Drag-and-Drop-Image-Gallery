fd12d0ff21996a6a7eabffc42f98fa23d1d926a2
Jolextom <goldenprince724@gmail.com>
2023-09-21 23:27:56 +0100

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,10 +1,10 @@
-const apiKey = import.meta.env.VITE_API_KEY;
-const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
-const projectId = import.meta.env.VITE_PROJECT_ID;
-const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
-const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
-const appId = import.meta.env.VITE_APP_ID;
-const measurementId = import.meta.env.VITE_MEASUREMENT_ID;
+// const apiKey = import.meta.env.VITE_API_KEY;
+// const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
+// const projectId = import.meta.env.VITE_PROJECT_ID;
+// const storageBucket = import.meta.env.VITE_STORAGE_BUCKET;
+// const messagingSenderId = import.meta.env.VITE_MESSAGING_SENDER_ID;
+// const appId = import.meta.env.VITE_APP_ID;
+// const measurementId = import.meta.env.VITE_MEASUREMENT_ID;
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -19,13 +19,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
-  measurementId: measurementId,
+  apiKey: "AIzaSyBd25nd09gpOUCz6FRQ73yRaXM1sKfHshg",
+  authDomain: "drag-and-drop-image-uplo-95ae2.firebaseapp.com",
+  projectId: "drag-and-drop-image-uplo-95ae2",
+  storageBucket: "drag-and-drop-image-uplo-95ae2.appspot.com",
+  messagingSenderId: "172823590569",
+  appId: "1:172823590569:web:cf6145c29a1cd93aecdbb8",
+  measurementId: "G-9HRYD1QD0C",
 };
 
 // Initialize Firebase
